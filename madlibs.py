679c69cf99ad1e75eb9f0360c9d6145e4620b26f
mjmeador <157657938+mjmeador@users.noreply.github.com>
2024-01-30 00:22:32 +0000

    madlibs done

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,18 @@
+noun = input ("Enter a noun: ")
+adjective = input ("Enter an adjective: ")
+verb = input ("Enter a verb: ")
+
+print(f"{noun} was {adjective} while {verb}")
+cat = input ("Enter a cat: ")
+status = input ("Enter a status: ")
+color = input ("Enter a color: ")
+action = input ("Enter an action: ")
+weapon = input ("Enter a weapon: ")
+sound = input ("Enter a sound: ")
+person = input ("Enter a person: ")
+name = input ("Enter a name: ")
+setting = input ("Enter a setting: ")
+aftermath = input ("Enter an aftermath: ")
+print(f"the {color} {cat} was {status} while {action} with {weapon} that went {sound} while a {person} named {name} ran to {setting")
+
 # TODO Create your inputs, variables, and add your story here!
